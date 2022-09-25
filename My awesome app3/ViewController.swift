66f581e57f55fd6fe87055f90c0919455a7ee7de
Steve Diamond <spsc@bellsouth.net>
2022-09-25 12:31:44 -0400

    Initial Commit

---
+++ My awesome app3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My awesome app3
+//
+//  Created by Steven Diamond on 9/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
